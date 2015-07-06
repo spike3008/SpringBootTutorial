fb2a67a296244d9c6010061d79591efcc4b54c2c
Darda, Marcin <marcin.darda@mobica.com>
2015-07-06 16:21:16 +0200

    Initial commit

---
+++ src/main/java/ToDoAppConfig.java
@@ -0,0 +1,14 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class ToDoAppConfig {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoAppConfig.class, args);
+    }
+}
